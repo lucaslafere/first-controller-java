0d0898c510e5a4364dae66d808f49646a4c8ed9c
Lucas Lafèré <laferelucas@gmail.com>
2023-01-18 11:58:07 -0300

    added car model

---
+++ src/main/java/com/helloworld/api/model/Car.java
@@ -0,0 +1,10 @@
+package com.helloworld.api.model;
+
+public class Car {
+    private Long id;
+    private String modelo;
+    private String fabricante;
+    private String dataFabricacao;
+    private int valor;
+    private int anoModelo;
+}
