01d854f32c860f0dab7ca0fb7783781f16e0929a
Lucas Lafèré <laferelucas@gmail.com>
2023-01-18 11:39:03 -0300

    fixed controller syntax

--- src/main/java/com/helloworld/api/controller/HelloController.java
+++ src/main/java/com/helloworld/api/controller/HelloController.java
@@ -3,13 +3,11 @@ package com.helloworld.api.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController;
-@RequestMapping'("/hello")
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
-    
+
     @GetMapping   
     public String HelloWorld () {
         
