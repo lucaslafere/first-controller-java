ae0718fb52bfe49669d5303e496ee355053ebfcc
Lucas Lafèré <laferelucas@gmail.com>
2023-01-18 11:37:42 -0300

    added route mapping

--- src/main/java/com/helloworld/api/controller/HelloController.java
+++ src/main/java/com/helloworld/api/controller/HelloController.java
@@ -1,8 +1,18 @@
 package com.helloworld.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController;
+@RequestMapping'("/hello")
 public class HelloController {
-       
+    
+    @GetMapping   
     public String HelloWorld () {
+        
         return "Hello world!";
     }
 }
