6792effe556cba78f9182ec4acf79ed2fa7bd9ca
Lucas Lafèré <laferelucas@gmail.com>
2023-01-19 16:34:44 -0300

    added types to car model default msg

--- src/main/java/com/helloworld/api/controller/DefaultController.java
+++ src/main/java/com/helloworld/api/controller/DefaultController.java
@@ -10,6 +10,6 @@ public class DefaultController {
     
     @GetMapping
     public String Default () {
-        return "This is the main page. I recommend trying a get on /hello. You could also get /api/car to get a list of all cards published, and a post to /api/car to post your car. /n A car needs a 'modelo', 'fabricante', 'dataFabricacao', 'valor' and 'anoModelo'";
+        return "This is the main page. I recommend trying a get on /hello. You could also get /api/car to get a list of all cards published, and a post to /api/car to post your car. /n A car needs a String: 'modelo', String: 'fabricante', String: 'dataFabricacao', double: 'valor' and int: 'anoModelo'";
     }
 }
