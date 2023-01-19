af2cd6d7ca0cc22be4ecaf6313370ea0ed481dba
Lucas Lafèré <laferelucas@gmail.com>
2023-01-19 16:31:38 -0300

    added default route mapping

--- src/main/java/com/helloworld/api/controller/DefaultController.java
+++ src/main/java/com/helloworld/api/controller/DefaultController.java
@@ -1,5 +1,15 @@
 package com.helloworld.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class DefaultController {
     
+    @GetMapping
+    public String Default () {
+        return "This is the main page";
+    }
 }
